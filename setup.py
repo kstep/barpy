c47359461ca3dedf0d741dd6eb7752ca9bea3eb5
Konstantin Stepanov <me@kstep.me>
2013-01-05 17:51:07 +0300

    Wrapped `open` in setup into maybe to avoid unnecessary setup failures

--- setup.py
+++ setup.py
@@ -5,11 +5,40 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+class Nothing(object):
+    def __call__(self, *args, **kw):
+        return self
+    def __getattr__(self, name):
+        return self
+    def __setattr__(self, name, value):
+        pass
+    def __delattr__(self, name):
+        pass
+
+    def __str__(self):
+        return ''
+    __repr__ = __str__
+
+    def __unicode__(self):
+        return u''
+    def __len__(self):
+        return 0
+
+nothing = Nothing()
+
+def maybe(func):
+    def wrapper(*args, **kw):
+        try:
+            return func(*args, **kw)
+        except:
+            return nothing
+    return wrapper
+
 setup(name="barpy",
-      version="0.1.1",
+      version="0.1.2",
       url="http://github.com/kstep/barpy/",
       description="Ruby barby barcodes generating library port to Python",
-      long_description=open("README.txt").read(),
+      long_description=maybe(open)("README.txt").read(),
       author="Konstantin Stepanov",
       author_email="me@kstep.me",
       license="BSD",
